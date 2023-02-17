31203a8169187a346420f9c6d4cf38ee6ed8ade3
J Seymour <JustJakeSeymour@gmail.com>
2023-02-16 21:10:21 -0700

    start writing tests

---
+++ app/helpers/signs_helper.rb
@@ -0,0 +1,18 @@
+module SignsHelper
+  def zodiac_signs
+    [
+      ['Aries', 'aries'], 
+      ['Taurus', 'taurus'],
+      ['Gemini', 'gemini'], 
+      ['Cancer', 'cancer'], 
+      ['Leo', 'leo'], 
+      ['Virgo', 'virgo'],
+      ['Libra', 'libra'], 
+      ['Scorpio', 'scorpio'], 
+      ['Sagittarius', 'sagittarius'], 
+      ['Capricorn', 'capricorn'], 
+      ['Aquarius', 'aquarius'],
+      ['Pisces', 'pisces']
+    ]
+  end
+end
